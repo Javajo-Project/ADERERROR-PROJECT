c282226464d2cea2d39fb8af3eb644a1ea68d4fb
MOONWUD <moonqazwsx@naver.com>
2024-05-17 17:54:47 +0900

    240517-productPage-문지영

---
+++ productPage.js
@@ -0,0 +1,19 @@
+const Swiper = new Swiper(".mo-img", {
+  speed: 600,
+  loop:true,
+  parallax: true, 
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: "swiper-pagination",
+    type: "fraction",
+  }
+});
+
+
+$('.subTextBox-item .subTextBox-item-title').on('click', (e) => {
+    $(e.target).next('p').slideToggle()
+  });
+
