355a0535d937093f0f8d8e2280697d381368faf6
MOONWUD <moonqazwsx@naver.com>
2024-05-20 13:05:18 +0900

    240520-global-문지영

--- productPage.js
+++ productPage.js
@@ -1,16 +1,3 @@
-// const Swiper = new Swiper(".mo-img", {
-//   speed: 600,
-//   loop:true,
-//   parallax: true, 
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false
-//   },
-//   pagination: {
-//     el: "swiper-pagination",
-//     type: "fraction",
-//   }
-// });
 
 
 $('.subTextBox-item .subTextBox-item-title').on('click', (e) => {
