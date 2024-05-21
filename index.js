0e33963cd06a4d7ea9f9f1faf95ff5c75d9f4d45
EunhyeHa <grace9695@naver.com>
2024-05-21 16:50:22 +0900

    240521-brand-하은혜

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
+/****** index mainTxt show ******/
+
+document.addEventListener("DOMContentLoaded", function () {
+    var pElement = document.querySelector(".index-main-text .show1");
+
+    function showP() {
+        setTimeout(function () {
+            pElement.classList.add("show");
+        }, 1);
+    }
+
+    showP();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var pElement = document.querySelector(".index-main-text .show2");
+
+    function showP() {
+        setTimeout(function () {
+            pElement.classList.add("show");
+        }, 1);
+    }
+
+    showP();
+});
+
+
 /****** index main Bottom ******/
 /****** 곡선 > 직선 이벤트 ******/
 
 gsap.registerPlugin(ScrollTrigger);
-
 gsap.to("#shape", {
-  scrollTrigger: {
-    trigger: "#shape",
-    start: "top 50%", // 요소의 상단이 닿을 때 스크롤 이벤트 트리거
-    end: "bottom 80%", // 요소의 하단이 닿을 때 스크롤 이벤트 트리거
-    scrub: true,
-    markers: false
-  },
-  width: "120%",
-  height: "1000px",
-  borderRadius: "0%",
-  duration: 2
+    scrollTrigger: {
+        trigger: "#shape",
+        start: "top 30%", // 요소의 상단이 닿을 때 스크롤 이벤트 트리거
+        end: "bottom 80%", // 요소의 하단이 닿을 때 스크롤 이벤트 트리거
+        scrub: true,
+        markers: true
+    },
+    width: "120%",
+    height: "800px",
+    borderRadius: "0%",
+    duration: 2
 });
 
 
 /****** index horizontal lookbook ******/
 
-const container=document.querySelector('.index-collabo-horizontal');
+const container = document.querySelector('.index-collabo-horizontal');
 const sections = gsap.utils.toArray('.index-collabo-horizontal .index-collabo-horizontalwrapper1');
-const texts=gsap.utils.toArray('.index-collabo-horizontaltext');
+const texts = gsap.utils.toArray('.index-collabo-horizontaltext');
 
-let scrollTween=gsap.to(sections,{
-    xPercent:-100*(sections.length-1),
-    ease:"none",
-    scrollTrigger:{
-        trigger:".index-collabo-horizontal",
-        pin:true,
-        scrub:1,
-        end:"+=3000",
+let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".index-collabo-horizontal",
+        pin: true,
+        scrub: 1,
+        end: "+=3000",
     }
 })
-sections.forEach(section=>{
-    let text=section.querySelector('.index-collabo-horizontaltext');
-    
-    gsap.from(text,{
-        y:-130,
-        opacity:0,
-        duration:1,
-        ease:"none",
-        stagger:0.5,
-        scrollTrigger:{
-            trigger:section,
-            containerAnimation:scrollTween,
-            start:"left center",
-            markers:false
+sections.forEach(section => {
+    let text = section.querySelector('.index-collabo-horizontaltext');
+
+    gsap.from(text, {
+        y: -130,
+        opacity: 0,
+        duration: 1,
+        ease: "none",
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: section,
+            containerAnimation: scrollTween,
+            start: "left center",
+            markers: false
         }
     })
 })
@@ -71,7 +97,7 @@ gsap.to(".slogan-wrapper", {
     duration: 1,
     opacity: 1,
     y: -250,
-       ease: "easeOutExpo",
+    ease: "easeOutExpo",
     scrollTrigger: {
         trigger: "#slogan",
         start: "top bottom",
