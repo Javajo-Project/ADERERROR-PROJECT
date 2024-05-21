c545e908446b0b451f8e2035df3e21e688029602
MOONWUD <moonqazwsx@naver.com>
2024-05-21 16:48:41 +0900

    240521-productPage-문지영

--- productPage.js
+++ productPage.js
@@ -4,4 +4,22 @@ $('.subTextBox-item .subTextBox-item-title').on('click', (e) => {
   });
 
 
+  const mq = matchMedia('(max-width: 430px)');
+  const imgChange = () => $('.page-sidebarImages-wrapper li').first().appendTo('.page-sidebarImages-wrapper')
+  if(mq.matches) {
+  setInterval(imgChange, 3000);
+} else {
+  clearInterval(imgChange);
+}
 
+// const view = (bg, txt) => {
+//   document.body.style.backgroundColor = bg;
+//   document.body.innerHTML = `<h2>768${txt} 화면</h2>`;
+// }
+
+// const resizeEx = () => {
+//   console.log('리사이즈 됨');
+//   mq.matches ? view('orange', '이하') : view('lightblue', '이상');
+// }
+
+// addEventListener('resize', resizeEx);
